42a06344d0f21d252a1bfebbf1d2b5c53917d34a
nguyenvv4 <nguyenvv4@fpt.edu.vn>
2023-02-06 10:09:49 +0700

    update

---
+++ luyentap-spa/sinhvien.js
@@ -0,0 +1,11 @@
+window.SinhVienController = function ($scope, $http) {
+  $scope.listSinhVien = [];
+  $http.get("http://localhost:3000/sinhviens").then(
+    function (response) {
+      $scope.listSinhVien = response.data;
+    },
+    function (error) {
+      console.log("Error: " + error);
+    }
+  );
+};
