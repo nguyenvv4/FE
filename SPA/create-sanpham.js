d6af54fe75ebec66ccf8138f57b7966c01c1b2cc
nguyenvv4 <nguyenvv4@fpt.edu.vn>
2023-02-10 13:02:48 +0700

    update

--- SPA/create-sanpham.js
+++ SPA/create-sanpham.js
@@ -1,21 +1,16 @@
 window.CreateSanPhamController = function ($scope, $http, $location) {
-  $scope.id = "";
-  $scope.tenSp = "";
-  $scope.soLuongTon = "";
-  $scope.giaBan = "";
-  $scope.giamGia = "";
   $scope.onSubmit = function () {
     $http
       .post("http://localhost:3000/sanphams", {
         id: $scope.id,
         tenSp: $scope.tenSp,
-        soLuongTon: $scope.soLuongTon,
         giaBan: $scope.giaBan,
+        soLuongTon: $scope.soLuongTon,
         giamGia: $scope.giamGia,
       })
       .then(function (response) {
         if (response.status === 201) {
-          alert("Them thanh cong");
+          alert("Thêm sản phẩm thành công");
           $location.path("/home");
         }
       });
